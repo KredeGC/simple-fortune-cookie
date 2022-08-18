19451df07cf973aa9d2e100d57b8029a65420077
Christian Brügger <chbru20@student.sdu.dk>
2022-08-18 18:39:32 +0000

    Change integration test to use proper ports

--- curl-test.sh
+++ curl-test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-http_code=$(curl -s -o /dev/null -w '%{http_code}'  http://inst67.z1.eficode.academy:8080;)
+http_code=$(curl -s -o /dev/null -w '%{http_code}'  http://inst67.z1.eficode.academy:8082;)
 
 if [[ $http_code -eq 200 ]]; then
-    echo "http://inst67.z1.eficode.academy:8080 responded with 200"
+    echo "http://inst67.z1.eficode.academy:8082 responded with 200"
     exit 0
 fi
 
-echo "Could not reach http://inst67.z1.eficode.academy:8080, responded with $http_code"
+echo "Could not reach http://inst67.z1.eficode.academy:8082, responded with $http_code"
 exit 14
